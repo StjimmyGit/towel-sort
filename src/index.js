f77cf21487ac00f34e25bba00400d7ee55a420fc
Ilya <admin@vodokanal.local>
2022-01-24 10:50:02 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort(matrix) {
+
+  if (matrix) {
+    return matrix.reduce((acc, cur, i) => {
+      return acc.concat(((i % 2 == 0) ? cur : cur.reverse()));
+    }, []);
+  } else {
+    return [];
+  }
+
+
+};
+
